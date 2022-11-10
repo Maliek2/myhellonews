48a1ea6d9ffd99e3a74df086e882745b2dbbd350
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-10 03:34:18 +0200

    Create InArticle.vue

---
+++ src/views/InArticle.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="home VueCurrencyFilter">
+    <section class="container hero">
+      <b-row mb="4" align="center" justify="center" direction="column">
+        <h1 as="h1" fontSize="5xl">In Article Adsense</h1>
+      </b-row>
+    </section>
+
+    <section class="container">
+      <p :mb="2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis in nulla porttitor ultrices. Sed dictum ante sit amet
+        libero aliquam, vitae bibendum nisi blandit. Etiam id euismod est. Sed varius tristique libero quis porta. Nam ex ligula, semper eu
+        nisl at, interdum viverra nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin
+        vehicula commodo mauris. Sed eu blandit sem, hendrerit placerat dui. Maecenas non ante ante. Sed et mi ut quam porttitor egestas id
+        in sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eros nulla,
+        pulvinar id ante nec, vulputate bibendum lectus.
+      </p>
+      <p :mb="2">
+        Suspendisse dapibus sodales lorem sed pharetra. Nam pellentesque posuere lorem, sed vulputate mi varius dapibus. Suspendisse
+        potenti. Nulla non interdum libero. Duis ornare risus non leo posuere maximus. Fusce id urna in sapien tincidunt finibus nec eget
+        diam. Vivamus vitae efficitur lacus. Curabitur vel ex urna. In scelerisque lorem tellus, nec tempus magna egestas vel. In eu erat
+        vestibulum, euismod est eget, elementum felis.
+      </p>
+      <p :mb="2">
+        Duis nunc erat, ultricies sed tristique quis, luctus id risus. Aliquam erat volutpat. Praesent tincidunt ex urna, quis luctus tortor
+        consectetur sit amet. Phasellus ullamcorper eget sem convallis condimentum. Vivamus feugiat nisl in blandit imperdiet. Nulla laoreet
+        sollicitudin orci, at maximus mi posuere fermentum. Quisque porttitor leo nec purus fermentum congue. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Nam in nibh at lacus consequat congue nec nec lacus.
+      </p>
+      <p :mb="2">
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel posuere velit, et volutpat tortor.
+        Nullam non efficitur neque, id egestas mauris. Vivamus vehicula eget nisi nec pulvinar. In et arcu dapibus, condimentum felis quis,
+        mollis nibh. Duis sem eros, placerat sit amet eros porttitor, efficitur gravida augue. Donec non imperdiet enim. Cras ac consequat
+        est.
+      </p>
+      <p :mb="2">
+        Vestibulum congue convallis nisi nec vulputate. Pellentesque posuere suscipit quam, non vulputate augue porta ac. Ut quam justo,
+        iaculis sed ipsum imperdiet, eleifend fermentum tortor. Nam facilisis imperdiet sollicitudin. Proin ultrices nisl vel odio congue,
+        quis ornare turpis blandit. Proin felis odio, luctus sit amet urna id, luctus venenatis ligula. Maecenas tincidunt pulvinar diam
+        vitae suscipit. Suspendisse feugiat mattis elit a interdum. Fusce fermentum fringilla odio non finibus. Integer pretium rutrum
+        ligula, ut fringilla mauris facilisis dapibus. Integer mattis leo vel tincidunt porta.
+      </p>
+
+      <div class="ads">
+        <b-row align="center" justify="center" direction="column">
+          <c-box w="100%" overflow="hidden">
+            <InArticleAdsense style="width: 100%; min-width: 250px" data-ad-client="ca-pub-5442972248172818" data-ad-slot="7974047383" />
+          </c-box>
+        </b-row>
+      </div>
+
+      <p :mb="2">
+        Duis nunc erat, ultricies sed tristique quis, luctus id risus. Aliquam erat volutpat. Praesent tincidunt ex urna, quis luctus tortor
+        consectetur sit amet. Phasellus ullamcorper eget sem convallis condimentum. Vivamus feugiat nisl in blandit imperdiet. Nulla laoreet
+        sollicitudin orci, at maximus mi posuere fermentum. Quisque porttitor leo nec purus fermentum congue. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Nam in nibh at lacus consequat congue nec nec lacus.
+      </p>
+      <p :mb="2">
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel posuere velit, et volutpat tortor.
+        Nullam non efficitur neque, id egestas mauris. Vivamus vehicula eget nisi nec pulvinar. In et arcu dapibus, condimentum felis quis,
+        mollis nibh. Duis sem eros, placerat sit amet eros porttitor, efficitur gravida augue. Donec non imperdiet enim. Cras ac consequat
+        est.
+      </p>
+
+      <b-row>
+        <p :mb="2" :mr="5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis in nulla porttitor ultrices. Sed dictum ante sit amet
+          libero aliquam, vitae bibendum nisi blandit. Etiam id euismod est. Sed varius tristique libero quis porta. Nam ex ligula, semper
+          eu nisl at, interdum viverra nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin
+          vehicula commodo mauris. Sed eu blandit sem, hendrerit placerat dui. Maecenas non ante ante. Sed et mi ut quam porttitor egestas
+          id in sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eros nulla,
+          pulvinar id ante nec, vulputate bibendum lectus.
+        </p>
+
+        <b-row align="center" justify="center" direction="column">
+          <c-box minW="sm" overflow="hidden">
+            <InArticleAdsense style="min-width: 250px" data-ad-client="ca-pub-5442972248172818" data-ad-slot="2669016517" />
+          </c-box>
+        </b-row>
+
+        <p :mb="2" :ml="5">
+          Suspendisse dapibus sodales lorem sed pharetra. Nam pellentesque posuere lorem, sed vulputate mi varius dapibus. Suspendisse
+          potenti. Nulla non interdum libero. Duis ornare risus non leo posuere maximus. Fusce id urna in sapien tincidunt finibus nec eget
+          diam. Vivamus vitae efficitur lacus. Curabitur vel ex urna. In scelerisque lorem tellus, nec tempus magna egestas vel. In eu erat
+          vestibulum, euismod est eget, elementum felis.
+        </p>
+      </b-row>
+
+      <p :mb="2">
+        Duis nunc erat, ultricies sed tristique quis, luctus id risus. Aliquam erat volutpat. Praesent tincidunt ex urna, quis luctus tortor
+        consectetur sit amet. Phasellus ullamcorper eget sem convallis condimentum. Vivamus feugiat nisl in blandit imperdiet. Nulla laoreet
+        sollicitudin orci, at maximus mi posuere fermentum. Quisque porttitor leo nec purus fermentum congue. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Nam in nibh at lacus consequat congue nec nec lacus.
+      </p>
+      <p :mb="2">
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel posuere velit, et volutpat tortor.
+        Nullam non efficitur neque, id egestas mauris. Vivamus vehicula eget nisi nec pulvinar. In et arcu dapibus, condimentum felis quis,
+        mollis nibh. Duis sem eros, placerat sit amet eros porttitor, efficitur gravida augue. Donec non imperdiet enim. Cras ac consequat
+        est.
+      </p>
+      <p :mb="2">
+        Vestibulum congue convallis nisi nec vulputate. Pellentesque posuere suscipit quam, non vulputate augue porta ac. Ut quam justo,
+        iaculis sed ipsum imperdiet, eleifend fermentum tortor. Nam facilisis imperdiet sollicitudin. Proin ultrices nisl vel odio congue,
+        quis ornare turpis blandit. Proin felis odio, luctus sit amet urna id, luctus venenatis ligula. Maecenas tincidunt pulvinar diam
+        vitae suscipit. Suspendisse feugiat mattis elit a interdum. Fusce fermentum fringilla odio non finibus. Integer pretium rutrum
+        ligula, ut fringilla mauris facilisis dapibus. Integer mattis leo vel tincidunt porta.
+      </p>
+
+      <div class="ads">
+        <b-row align="center" justify="center" direction="column">
+          <c-box w="100%" overflow="hidden">
+            <InArticleAdsense style="width: 100%; min-width: 250px" data-ad-client="ca-pub-5442972248172818" data-ad-slot="7974047383" />
+          </c-box>
+        </b-row>
+      </div>
+    </section>
+
+    <section class="container">
+      <c-stack :spacing="5" is-inline>
+        <Box
+          title="Default Adsense"
+          description="A simple way to get ads on your page. Choose the size, placement and style you want to display."
+          link="https://support.google.com/adsense/answer/6002575?hl=en_GB"
+          demo="/"
+        />
+
+        <Box
+          title="In Feed Adsense"
+          description="Ads that flow naturally inside a list of articles or products on your site, offering a great user experience."
+          link="https://support.google.com/adsense/answer/7171765?hl=en_GB"
+          demo="/in-feed/"
+        />
+      </c-stack>
+    </section>
+
+    <section class="container ads">
+      <b-row align="center" justify="center" direction="column">
+        <c-box w="100%" border-width="1px" rounded="lg" overflow="hidden">
+          <Adsense
+            style="width: 100%; min-width: 250px"
+            ins-style="display:inline-block;width:728px;height:90px"
+            data-ad-client="ca-pub-5442972248172818"
+            data-ad-slot="7319719723"
+          >
+          </Adsense>
+        </c-box>
+      </b-row>
+    </section>
+
+    <footer class="footer">
+      <b-row m="2" align="center" justify="center">
+        <p textAlign="center">Built with ❤️ by <a href="https://mazipan.space">Irfan Maulana</a></p>
+      </b-row>
+    </footer>
+  </div>
+</template>
+
+<script lang="js">
+import Box from '../components/Box'
+
+export default {
+  name: 'InArticle',
+  components: {
+    Box
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+pre:not(:last-of-type) {
+  margin-bottom: 2rem;
+}
+
+.container {
+  margin-top: 2em;
+
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  @media (min-width: 760px) {
+    padding-left: 14rem;
+    padding-right: 14rem;
+  }
+}
+</style>
